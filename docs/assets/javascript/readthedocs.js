eda40b55ed9a2fde51c35a26a3da2f0f7cd1ad2b
austinRon <becker.wiliams@gmail.com>
2025-08-26 16:43:22 -0500

    readthedocs enablement - Fixed incorrect suffix, moving `docs` -> `_readthedocs`
    *** CHECKPOINT ***
    
    ~rbw

--- docs/assets/javascript/readthedocs.js
+++ docs/assets/javascript/readthedocs.js
@@ -1,10 +1,3 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-// Trigger Read the Docs' search addon instead of Material MkDocs default
-    document.querySelector(".md-search__input").addEventListener("focus", (e) => {
-        const event = new CustomEvent("readthedocs-search-show");
-        document.dispatchEvent(event);
-    });
-});
 // Use CustomEvent to generate the version selector
 document.addEventListener(
     "readthedocs-addons-data-ready",
